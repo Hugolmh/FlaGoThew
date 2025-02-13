c80fdd93d4fee4a77107feffeca71d0fe470f97c
KinjerJS <bipkinjer@gmail.com>
2025-02-13 15:46:50 +0100

    feat: add swap form util

---
+++ FlaGoThew/model/utils/FormUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlaGoThew.model.utils
+{
+    public class FormUtil
+    {
+        public static void SwapForm(Form current, Form next)
+        {
+            current.Hide();
+            next.Show();
+            next.Closed += (s, args) => current.Close();
+        }
+
+    }
+}
