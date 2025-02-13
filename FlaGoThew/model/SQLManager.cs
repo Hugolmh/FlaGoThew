a4944c70ed066ae3fd8c535e84885a1304a01100
KinjerJS <bipkinjer@gmail.com>
2025-02-13 14:17:24 +0100

    fix: add documentations and modify namespace

--- FlaGoThew/model/SQLManager.cs
+++ FlaGoThew/model/SQLManager.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -6,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ListeDeCourseCours.model
+namespace GestionTache.sql
 {
-    internal class SQLManager
+    public class SQLManager
     {
         private const string connectionUrl = "server=localhost;Port=3306; database=listecourse;uid=root;pwd=\"\";";
 
+        
         public static MySqlDataReader query(string query)
         {
             return query<MySqlDataReader>((cnn) =>
@@ -32,6 +34,31 @@ namespace ListeDeCourseCours.model
             });
         }
 
+        /// <summary>
+        /// Permet de simplifier l'appel de requetes vers la base de données.
+        /// Elle ouvre et ferme les connection entre chaque requetes automatiquement.
+        /// Exemple de code :
+        /// <code>
+        ///    SQLManager.query<int>((cnn) =>
+        ///    {
+        ///        MySqlCommand cmd = cnn.CreateCommand();
+        ///        cmd.CommandText = "SELECT id FROM maTable WHERE id=@id";
+        ///        cmd.Parameters.AddWithValue("@id", 10);
+        ///        MySqlDataReader result = cmd.ExecuteReader();
+        ///        return result.NextResult()? result.GetInt32("id") : -1;
+        ///    });
+        /// </code>
+        /// Le code d'exemple fait une requete d'un simple SELECT en lui passant en paramettre 
+        /// du nom "@id" un int '10'.
+        /// Elle retourne un int, celle de l'id du resultat de la requete.
+        /// Dans le lambda, on y retourne directement le resultat que l'ont souhaite retourner à
+        /// la fin de la requete tout en y spécifiant le type de retour dans la généricité '<int>'
+        /// précisé dans l'appel de la requette.
+        /// 
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="ac"></param>
+        /// <returns></returns>
         public static R query<R>(Func<MySqlConnection, R> ac)
         {
 
